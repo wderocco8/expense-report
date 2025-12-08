204db8a890ef8e4a0b1a4d854d1868bc9d81fd85
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-12-07 22:07:32 -0500

    improved drizzle schema

--- schema.ts
+++ schema.ts
@@ -7,7 +7,39 @@ import {
   date,
   pgEnum,
   jsonb,
+  integer,
 } from "drizzle-orm/pg-core";
+
+// ------------ Drizzle table schema definitions ------------
+
+const status = pgEnum("status", [
+  "pending",
+  "processing",
+  "complete",
+  "failed",
+]);
+
+export const expenseReportJobsTable = pgTable("expense_report_jobs_table", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  // userId: uuid("user_id").notNull().defaultRandom(), // TODO
+  status: status("status"),
+  totalFiles: integer("total_files").notNull().default(0),
+  processedFiles: integer("processed_files").notNull().default(0),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const receiptFilesTable = pgTable("receipt_files_table", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  jobId: uuid("job_id")
+    .references(() => expenseReportJobsTable.id)
+    .notNull(),
+  s3Url: text("s3_url").notNull(),
+  status: status("status"),
+  errorMessage: text("error_message"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  processedAt: timestamp("processed_at"),
+});
 
 const categoryEnum = pgEnum("category", [
   "tolls/parking",
@@ -20,9 +52,11 @@ const categoryEnum = pgEnum("category", [
   "misc",
 ]);
 
-// Example table for receipts
-export const receipts = pgTable("receipts", {
+export const extractedExpensesTable = pgTable("extracted_expenses_table", {
   id: uuid("id").primaryKey().notNull().defaultRandom(),
+  receiptId: uuid("receipt_id")
+    .references(() => receiptFilesTable.id)
+    .notNull(),
   merchant: text("merchant"),
   description: text("description"),
   date: date("date"),
@@ -32,9 +66,17 @@ export const receipts = pgTable("receipts", {
     mode: "train" | "car" | "plane" | null;
     mileage: number | null;
   } | null>(),
+  rawJson: jsonb("raw_json"),
+  modelVersion: text("model_version").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
-// Type-safe helpers
-export type Receipt = typeof receipts.$inferSelect;
-export type NewReceipt = typeof receipts.$inferInsert;
+// ------------ Type-safe helpers ------------
+export type ExtractedExpense = typeof extractedExpensesTable.$inferSelect;
+export type NewExtractedExpense = typeof extractedExpensesTable.$inferInsert;
+
+export type ExpenseReportJob = typeof expenseReportJobsTable.$inferSelect;
+export type NewExpenseReportJob = typeof expenseReportJobsTable.$inferInsert;
+
+export type ReceiptFile = typeof receiptFilesTable.$inferSelect;
+export type NewReceiptFile = typeof receiptFilesTable.$inferInsert;
