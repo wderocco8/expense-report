8e81590dad31a9cd73b5c886d9631d758bb49051
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-12-17 22:20:58 -0500

    added todo

--- server/services/storage.service.ts
+++ server/services/storage.service.ts
@@ -45,3 +45,18 @@ export function getPublicObjectUrl(key: string) {
   // AWS S3
   return `https://${S3_BUCKET}.s3.${process.env.S3_REGION}.amazonaws.com/${key}`;
 }
+
+// TODO: look into this
+// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+// import { GetObjectCommand } from "@aws-sdk/client-s3";
+
+// export async function getSignedReceiptUrl(key: string) {
+//   return getSignedUrl(
+//     s3,
+//     new GetObjectCommand({
+//       Bucket: process.env.S3_BUCKET!,
+//       Key: key,
+//     }),
+//     { expiresIn: 60 * 5 }
+//   );
+// }
