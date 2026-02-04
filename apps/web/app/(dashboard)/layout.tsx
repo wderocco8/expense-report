c6fb2b00142b5ee8a9034a2cdbe2ea53a5fa684f
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2026-02-03 19:06:00 -0500

    added theme toggle (WIP)

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/sidebar";
 import { cookies } from "next/headers";
 import { getSession } from "@/lib/auth/session";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default async function DashboardLayout({
   children,
@@ -26,13 +27,16 @@ export default async function DashboardLayout({
       <AppSidebar user={user} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <DynamicBreadcrumb />
+          <div className="flex justify-between w-full mr-2">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <DynamicBreadcrumb />
+            </div>
+            <ModeToggle />
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
